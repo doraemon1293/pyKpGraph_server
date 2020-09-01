10eec1e1433351281f0e7beb1afe2049c4ba0ef3
323029@gmail.com <19241008o>
2020-09-01 09:20:57 +0100

    4g

--- operate_database.py
+++ operate_database.py
@@ -4,13 +4,9 @@ import os
 import collections
 import datetime
 import io
+import time
 
-
-def foo(row):
-    print(type(row["Time"]))
-
-
-def re_create_collection(MONGO_CLIENT_URL, DB_NAME, COLLECTIONS_NAME, unique_index=[], single_indice=[]):
+def re_create_collection(MONGO_CLIENT_URL, DB_NAME, COLLECTIONS_NAME, unique_index=[], single_indice=[],compound_indice=[]):
     # Create DB, drop DB if exists
     myclient = pymongo.MongoClient(MONGO_CLIENT_URL)
     dblist = myclient.list_database_names()
@@ -20,7 +16,7 @@ def re_create_collection(MONGO_CLIENT_URL, DB_NAME, COLLECTIONS_NAME, unique_ind
     # Create Collections
     if DB_NAME in dblist:
         mydb = myclient[DB_NAME]
-        print(("Database {} exits".format(DB_NAME)))
+        print(("Database {} existed".format(DB_NAME)))
     else:
         mydb = myclient[DB_NAME]
         print(("Database {} is created".format(DB_NAME)))
@@ -38,6 +34,12 @@ def re_create_collection(MONGO_CLIENT_URL, DB_NAME, COLLECTIONS_NAME, unique_ind
         temp = [(index, pymongo.ASCENDING) for index in unique_index]
         mycol.create_index(temp, unique=True, background=True)
 
+    # Create compound index
+    if compound_indice:
+        for compound_index in compound_indice:
+            temp = [(index, pymongo.ASCENDING) for index in compound_index]
+            mycol.create_index(temp, background=True)
+
     for index in single_indice:
         mycol.create_index(index, background=True)
 
@@ -51,6 +53,7 @@ def upsert_data(MONGO_CLIENT_URL, DB_NAME, COLLECTIONS_NAME, filename, skiprows,
     mydb = myclient[DB_NAME]
     mycol = mydb[COLLECTIONS_NAME]
     file_extention = os.path.splitext(filename)[1]
+    st=time.time()
     if file_extention == ".csv":
         # skip Total XXX Records
         import io
@@ -64,13 +67,22 @@ def upsert_data(MONGO_CLIENT_URL, DB_NAME, COLLECTIONS_NAME, filename, skiprows,
         df = pd.read_csv(temp, parse_dates=parse_dates, skiprows=skiprows, na_values=na_values)
     elif file_extention == ".xlsx":
         df = pd.read_excel(filename, parse_dates=parse_dates, na_values=na_values)
+    print("load",time.time()-st)
+    st=time.time()
+    for field in unique_index:
+        if field not in df.columns:
+            df[field]=None
+    print("add empty col",time.time()-st)
+    st=time.time()
     df["_id"] = df.apply(lambda row: "".join(
-        [row[field].strftime("%Y%m%d%H%M%S") if field in parse_dates else row[field] for field in unique_index]),
+        [row[field].strftime("%Y%m%d%H") if field in parse_dates else str(row[field]) for field in unique_index]),
                          axis=1)
-
+    print("add _id",time.time()-st)
+    st=time.time()
     df.columns = [x.replace(".", "_") for x in df.columns]
     data = df.to_dict(orient='records')  # Here's our added param..
-
+    print("trans df",time.time()-st)
+    st=time.time()
     auto_complete_field_to_add_sets = collections.defaultdict(set)
     bulk_operations = []
     for row in data:
@@ -79,8 +91,11 @@ def upsert_data(MONGO_CLIENT_URL, DB_NAME, COLLECTIONS_NAME, filename, skiprows,
                 auto_complete_field_to_add_sets[auto_complete_collection].add(row[auto_complete_field])
         filter = {"_id": row["_id"]}
         bulk_operations.append(pymongo.UpdateOne(filter, {"$set": row}, upsert=True))
+    print("generate bulk op",time.time()-st)
+    st=time.time()
     mycol.bulk_write(bulk_operations, ordered=False)
-
+    print("write bulk op",time.time()-st)
+    st=time.time()
     # upsert cell name/ gnodeb name /enodeb name etc....
     for auto_complete_collection in auto_complete_field_to_add_sets:
         d = [{"_id": v} for v in auto_complete_field_to_add_sets[auto_complete_collection]]
@@ -88,7 +103,8 @@ def upsert_data(MONGO_CLIENT_URL, DB_NAME, COLLECTIONS_NAME, filename, skiprows,
             mydb[auto_complete_collection].insert_many(d, ordered=False)
         except pymongo.errors.BulkWriteError as e:
             print(e.details)
-
+    print("write autocomplete col",time.time()-st)
+    st=time.time()
     print("upsert {} rows".format(len(data)))
 
     myclient.close()
--- run_operate_datebase.py
+++ run_operate_datebase.py
@@ -5,6 +5,7 @@ import pymongo
 import os
 import glob
 import zipfile
+import time
 
 st = time.time()
 MONGO_CLIENT_URL = "mongodb://localhost:27017/"
@@ -12,14 +13,75 @@ DB_NAME = "mydatabase"
 
 
 def recreate_collections():
-    operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "NR_CELL_NAMES")
-    operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "NR_GNODEB_NAMES")
-    operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "NR_CELLS_HOURLY",
-                                          unique_index=("Time", "Cell Name",),
-                                          single_indice=["Cell Name", "Time", "gNodeB Name"])
-    operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "NR_CELLS_DAILY",
-                                          unique_index=("Date", "Cell Name",),
-                                          single_indice=["Cell Name", "Date", "gNodeB Name"])
+    # 5G Colelctions
+    # operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "NR_CELL_NAMES")
+    # operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "NR_GNODEB_NAMES")
+    # operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "NR_CELLS_HOURLY",
+    #                                       compound_indice=[("Cell Name", "Time"), ("gNodeB Name", "Time")]
+    #                                       )
+    # operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "NR_CELLS_DAILY",
+    #                                       compound_indice=[("Cell Name", "Date"), ("gNodeB Name", "Date")]
+    #                                       )
+
+    # 4G Collections
+    operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "LTE_CELL_NAMES")
+    operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "LTE_ENODEB_NAMES")
+
+    operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "LTE_CELLS_HOURLY",
+                                          compound_indice=[("Cell Name", "Time"), ("eNodeB Name", "Time")]
+                                          )
+    operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "LTE_ETHS_HOURLY",
+                                          compound_indice=[("eNodeB Name", "Time")]
+                                          )
+
+    operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "LTE_CELLS_DAILY",
+                                          compound_indice=[("Cell Name", "Date"), ("eNodeB Name", "Date")]
+                                          )
+    operate_database.re_create_collection(MONGO_CLIENT_URL, DB_NAME, "LTE_ETHS_DAILY",
+                                          compound_indice=[("eNodeB Name", "Date")]
+                                          )
+
+
+def upgrade_4G_hourly_cell_data(fn, COLLECTIONS_NAME="LTE_CELLS_HOURLY"):
+    operate_database.upsert_data(MONGO_CLIENT_URL, DB_NAME, COLLECTIONS_NAME,
+                                 auto_complete_fields=[("Cell Name", "LTE_CELL_NAMES"),
+                                                       ("eNodeB Name", "LTE_ENODEB_NAMES")],
+                                 filename=fn,
+                                 parse_dates=["Time"],
+                                 skiprows=6,
+                                 unique_index=("Time", "Cell Name",),
+                                 )
+
+
+def upgrade_4G_hourly_eth_data(fn, COLLECTIONS_NAME="LTE_ETHS_HOURLY"):
+    operate_database.upsert_data(MONGO_CLIENT_URL, DB_NAME, COLLECTIONS_NAME,
+                                 auto_complete_fields=[("eNodeB Name", "LTE_ENODEB_NAMES")],
+                                 filename=fn,
+                                 parse_dates=["Time"],
+                                 skiprows=6,
+                                 unique_index=("Time", "eNodeB Name", "Cabinet No", "Port No", "Slot No", "Subrack No"),
+                                 )
+
+
+def upgrade_4G_daily_cell_data(fn, COLLECTIONS_NAME="LTE_CELLS_DAILY"):
+    operate_database.upsert_data(MONGO_CLIENT_URL, DB_NAME, COLLECTIONS_NAME,
+                                 auto_complete_fields=[("Cell Name", "LTE_CELL_NAMES"),
+                                                       ("eNodeB Name", "LTE_ENODEB_NAMES")],
+                                 filename=fn,
+                                 parse_dates=["Date"],
+                                 skiprows=6,
+                                 unique_index=("Date", "Cell Name",),
+                                 )
+
+
+def upgrade_4G_daily_eth_data(fn, COLLECTIONS_NAME="LTE_ETHS_DAILY"):
+    operate_database.upsert_data(MONGO_CLIENT_URL, DB_NAME, COLLECTIONS_NAME,
+                                 auto_complete_fields=[("eNodeB Name", "LTE_ENODEB_NAMES")],
+                                 filename=fn,
+                                 parse_dates=["Date"],
+                                 skiprows=6,
+                                 unique_index=("Date", "eNodeB Name", "Cabinet No", "Port No", "Slot No", "Subrack No"),
+                                 )
 
 
 def upgrade_5G_hourly_data(fn, COLLECTIONS_NAME="NR_CELLS_HOURLY"):
@@ -45,7 +107,7 @@ def upgrade_5G_daily_data(fn, COLLECTIONS_NAME="NR_CELLS_DAILY"):
 def update_data():
     if not os.path.exists(os.path.join("data", "extract")):
         os.makedirs(os.path.join("data", "extract"))
-
+    st = time.time()
     for fn in glob.glob(os.path.join("data", "*.zip")):
         for temp in glob.glob(os.path.join("data", "extract", "*")):
             os.remove(temp)
@@ -53,19 +115,30 @@ def update_data():
         with zipfile.ZipFile(fn, "r") as zipref:
             print("extracting {}".format(fn))
             zipref.extractall(os.path.join("data", "extract"))
-
+        print("extract", time.time() - st)
         for extract_fn in glob.glob(os.path.join("data", "extract", "*")):
             print("importing {}".format(extract_fn))
-            if "5G" in fn:
+            if "5G" in extract_fn:
                 if "Hour" in extract_fn:
                     upgrade_5G_hourly_data(extract_fn)
                 else:
                     upgrade_5G_daily_data(extract_fn)
+            if "4G" in extract_fn:
+                if "Hour" in extract_fn:
+                    if "BBU" in fn or "Eth" in fn:
+                        upgrade_4G_hourly_eth_data(extract_fn)
+                    else:
+                        upgrade_4G_hourly_cell_data(extract_fn)
+                else:
+                    if "BBU" in fn or "Eth" in fn:
+                        upgrade_4G_daily_eth_data(extract_fn)
+                    else:
+                        upgrade_4G_daily_eth_data(extract_fn)
 
-
-# recreate_collections()
+print("start:",time.time())
+recreate_collections()
 update_data()
-
+print("finish:",time.time())
 # col = pymongo.MongoClient(MONGO_CLIENT_URL)[DB_NAME][COLLECTIONS_NAME]
 # for doc in col.find({"Cell Name": "95021NCn781", "Time": datetime.datetime(2020, 4, 26, 0, 0)}):
 #     print(doc)
